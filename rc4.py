6a5e7a13e5d4c62b54920de3a10b5769eb275498
clanner <clan.clasher@yandex.com>
2015-04-14 07:15:20 +0000

    Create rc4.py

---
+++ rc4.py
@@ -0,0 +1,35 @@
+__all__=['RC4']
+"""
+The rc4 cipher.
+"""
+def convert_key(s):
+    return [ord(c) for c in s]
+
+def KSA(key):
+    keylength = len(key)
+
+    S = range(256)
+
+    j = 0
+    for i in range(256):
+        j = (j + S[i] + key[i % keylength]) % 256
+        S[i], S[j] = S[j], S[i]  # swap
+
+    return S
+
+def PRGA(S):
+    i = 0
+    j = 0
+    while True:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]  # swap
+
+        K = S[(S[i] + S[j]) % 256]
+        yield K
+
+def RC4(key):
+    if type(key)==str:
+        key= convert_key(key)
+    S = KSA(key)
+    return PRGA(S)
